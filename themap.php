1130fc4b0d444e6a1234994d2de84051acd2e394
Rob Landers <landers.robert@gmail.com>
2012-09-18 16:17:25 -0400

    Create the first php lines of the plugin

---
+++ themap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package TheMap
+ * @version 0.1
+ * @author Robert Landers (landers.robert@gmail.com)
+ */
+/*
+Plugin Name: Hello Dolly
+Plugin URI: http://wordpress.org/extend/plugins/themap/
+Description: Display your multiple locations easily using Bing Maps
+Author: Robert Landers
+Version: 0.1
+Author URI: http://withinboredom.info
+ */
+
+?>
